9a1ba3dde942fd77ca22c1a85033fe016d39b90c
agnysbueno <agnysbueno@hotmail.com>
2020-11-14 13:32:42 -0300

    creating order review

--- public/scripts/checkout.js
+++ public/scripts/checkout.js
@@ -24,3 +24,17 @@ function showDivs(n) {
     }
     dots[checkoutIndex-1].className += " selected";
 }
+
+function add(){
+    var atual = document.getElementById("total").value;
+    var novo = atual - (-1); //Evitando Concatenacoes
+    document.getElementById("total").value = novo;
+}
+  
+  function reduce(){
+    var atual = document.getElementById("total").value;
+    if(atual > 0) { //evita números negativos
+      var novo = atual - 1;
+      document.getElementById("total").value = novo;
+    }
+}
